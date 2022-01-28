81a016289cf0d81927fc0506afb64ab9271200dd
Rémy Frèrebeau <remy.frerebeau@systemathics.com>
2022-01-28 20:26:18 +0100

    Update dependencies

--- src/App.fs
+++ src/App.fs
@@ -274,7 +274,7 @@ let navbar =
             Bulma.navbarEnd.div [
                 Bulma.navbarItem.div [
                     Html.a [
-                        prop.href "https://github.com/ThisFunctionalTom/Xml2Feliz"
+                        prop.href "https://github.com/ThisFunctionalTom/Html2Feliz"
                         prop.children [
                             Bulma.icon [
                                 size.isSize2
